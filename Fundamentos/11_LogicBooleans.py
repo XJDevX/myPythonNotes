358003032906174536c2843cdef6b76536cb78c7
jxdeveloper <julianparrales19@gmail.com>
2024-04-01 19:33:26 -0500

    4/1/2024

---
+++ Fundamentos/11_LogicBooleans.py
@@ -0,0 +1,13 @@
+#<<---Logica de los booleanos--->>#
+
+#<<---AND--->>#
+and1 = True and True #-> True
+and2 = True and False #-> False
+and3 = False and True #-> False
+and4 = False and False #-> False
+
+#<<---OR--->>#
+or1 = True or True #-> True
+or2 = True or False #-> True
+or3 = False or True #-> True
+or4 = False or False #-> False
